4f94df7e521040382adcbe5f9dbd835bfcdd3368
Your Name <you@example.com>
2018-03-12 08:09:00 +0200

    IMX6 Var Dart 6ul Linux

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Mon Mar 12 07:19:15 IST 2018"
+#define LINUX_COMPILE_BY "elia"
+#define LINUX_COMPILE_HOST "elia-PC2"
+#define LINUX_COMPILER "gcc version 7.2.0 (GCC) "
